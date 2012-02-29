79806e0d528a3489865ff6e4212d4cbe0a0d449a
Devlin Liles <devlin@devlinliles.com>
2012-02-29 12:29:11 -0600

    Adding test for query logic

--- FrameworkExtension/FrameworkExtension.Core.Test/Given_A_Generic_Repository.cs
+++ FrameworkExtension/FrameworkExtension.Core.Test/Given_A_Generic_Repository.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using MSTest.AssertionHelpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
 
 namespace FrameworkExtension.Core.Test
 {
@@ -48,6 +49,22 @@ namespace FrameworkExtension.Core.Test
 
             //Assert
             repository.Context.IsByReferenceSame(context);
+        }
+
+        [TestMethod]
+        public void When_Given_A_Query_Object_Then_It_Executes_Against_Context()
+        {
+            //Arrange
+            var context = MockRepository.GenerateStrictMock<IDbContext>();
+            context.Expect(x => x.AsQueryable<Foo>()).Return(new List<Foo>().AsQueryable()).Repeat.Once();
+            var repository = new EntityFrameworkRepository<EFTestContext>(context);
+
+            //Act
+            repository.Find(new TestQuery());
+
+            //Assert
+            context.VerifyAllExpectations();
+
         }
     }
 

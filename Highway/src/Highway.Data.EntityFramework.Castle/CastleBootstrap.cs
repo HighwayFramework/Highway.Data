3e5369f32f28709b70b4f3528d25dd0261df5183
Devlin Liles <Devlin@devlinliles.com>
2012-06-28 22:32:47 -0500

    Adding folder structure for documentation, and castle bootstrap

---
+++ Highway/src/Highway.Data.EntityFramework.Castle/CastleBootstrap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Highway.Data.EntityFramework.Castle
+{
+    public class CastleBootstrap : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            
+        }
+    }
+}

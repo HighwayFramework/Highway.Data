a8279e09d231101272c07a5f4fde37f8e523207e
Devlin Liles <Devlin@devlinliles.com>
2012-06-28 23:45:18 -0500

    Updated documentation

---
+++ search/functions_69.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['install',['Install',['../class_highway_1_1_data_1_1_entity_framework_1_1_castle_1_1_castle_bootstrap.html#ac7161bb3c873e78b28b4085225c5a776',1,'Highway::Data::EntityFramework::Castle::CastleBootstrap']]]
+];

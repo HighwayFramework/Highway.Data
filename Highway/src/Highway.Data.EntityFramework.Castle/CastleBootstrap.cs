7cd14e1da2184b4480d06dde861990779acd7367
Devlin Liles <Devlin@devlinliles.com>
2012-06-28 22:41:54 -0500

    Fixing references from the project restructure

--- Highway/src/Highway.Data.EntityFramework.Castle/CastleBootstrap.cs
+++ Highway/src/Highway.Data.EntityFramework.Castle/CastleBootstrap.cs
@@ -8,8 +8,15 @@ using Castle.Windsor;
 
 namespace Highway.Data.EntityFramework.Castle
 {
+    /// <summary>
+    /// Castle specific bootstrap for installing types needed for usage to the current container
+    /// </summary>
     public class CastleBootstrap : IWindsorInstaller
     {
+        /// <summary>
+        /// Performs the installation in the <see cref="T:Castle.Windsor.IWindsorContainer"/>.
+        /// </summary>
+        /// <param name="container">The container.</param><param name="store">The configuration store.</param>
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
             

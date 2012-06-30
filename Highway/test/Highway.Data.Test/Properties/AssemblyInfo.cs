07738fcf6919ff160ce6b34cc6b09ca2aabbef8e
Tim Rayburn <tim@timrayburn.net>
2012-06-30 13:36:15 -0500

    Added SolutionInfo.cs to centralize version numbers and copyright info

--- Highway/test/Highway.Data.Test/Properties/AssemblyInfo.cs
+++ Highway/test/Highway.Data.Test/Properties/AssemblyInfo.cs
@@ -6,30 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Highway.Data.Tests")]
-[assembly: AssemblyDescription("The fastest and smoothest way to great architecture")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Devlin Liles and Tim Rayburn")]
-[assembly: AssemblyProduct("Highway.Data")]
-[assembly: AssemblyCopyright("Copyright © Devlin Liles 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("580c5ce5-f0d3-46e2-9f7d-dc1c8208990d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- Highway/test/MSTest.AssertionHelpers/Properties/AssemblyInfo.cs
+++ Highway/test/MSTest.AssertionHelpers/Properties/AssemblyInfo.cs
@@ -6,31 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MSTest.AssertionHelpers")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MSTest.AssertionHelpers")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("21906a8f-0a5c-43b5-abee-44f5656f8ccd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

8f23b5aa13d3a9dca361bcfff4717bba59dbd5e7
Devlin Liles <Devlin@devlinliles.com>
2012-07-05 11:09:18 -0500

    Publishing the test peice of Highway

--- Highway/src/Highway.Test.MSTest/Properties/AssemblyInfo.cs
+++ Highway/src/Highway.Test.MSTest/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MSTest.AssertionHelpers")]
+[assembly: AssemblyTitle("Highway.Test.MSTest")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("21906a8f-0a5c-43b5-abee-44f5656f8ccd")]

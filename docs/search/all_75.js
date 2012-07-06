0c3c44aa4ed3430eeb9aa9bd6c29f2156528c78e
Devlin Liles <Devlin@devlinliles.com>
2012-07-06 11:00:05 -0500

    Updating Documentation

--- docs/search/all_75.js
+++ docs/search/all_75.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['update_3c_20t_20_3e',['Update&lt; T &gt;',['../interface_highway_1_1_data_1_1_interfaces_1_1_i_data_context.html#a54acc7026404f8426c0e1efe6accaf26',1,'Highway.Data.Interfaces.IDataContext.Update&lt; T &gt;()'],['../class_highway_1_1_data_1_1_entity_framework_1_1_contexts_1_1_entity_framework_context.html#a3746422e9f70039d208c6dd6be02327e',1,'Highway.Data.EntityFramework.Contexts.EntityFrameworkContext.Update&lt; T &gt;()']]]
+  ['update_3c_20t_20_3e',['Update&lt; T &gt;',['../interface_highway_1_1_data_1_1_interfaces_1_1_i_data_context.html#a54acc7026404f8426c0e1efe6accaf26',1,'Highway.Data.Interfaces.IDataContext.Update&lt; T &gt;()'],['../class_highway_1_1_data_1_1_entity_framework_1_1_contexts_1_1_entity_framework_context.html#a3746422e9f70039d208c6dd6be02327e',1,'Highway.Data.EntityFramework.Contexts.EntityFrameworkContext.Update&lt; T &gt;()'],['../class_highway_1_1_data_1_1_entity_framework_1_1_contexts_1_1_entity_framework_object_context.html#afd7de00d4bfcaabfd92b71b34a7cfe33',1,'Highway.Data.EntityFramework.Contexts.EntityFrameworkObjectContext.Update&lt; T &gt;()']]]
 ];
--- docs/search/namespaces_68.js
+++ docs/search/namespaces_68.js
@@ -6,12 +6,15 @@ var searchData=
   ['entityframework',['EntityFramework',['../namespace_highway_1_1_data_1_1_entity_framework.html',1,'Highway::Data']]],
   ['eventmanagement',['EventManagement',['../namespace_highway_1_1_data_1_1_event_management.html',1,'Highway::Data']]],
   ['events',['Events',['../namespace_highway_1_1_data_1_1_interceptors_1_1_events.html',1,'Highway::Data::Interceptors']]],
+  ['factory',['Factory',['../namespace_highway_1_1_data_1_1_entity_framework_1_1_factory.html',1,'Highway::Data::EntityFramework']]],
   ['highway',['Highway',['../namespace_highway.html',1,'']]],
   ['interceptors',['Interceptors',['../namespace_highway_1_1_data_1_1_interceptors.html',1,'Highway::Data']]],
   ['interceptors',['Interceptors',['../namespace_highway_1_1_data_1_1_entity_framework_1_1_interceptors.html',1,'Highway::Data::EntityFramework']]],
   ['interfaces',['Interfaces',['../namespace_highway_1_1_data_1_1_interfaces.html',1,'Highway::Data']]],
   ['mappings',['Mappings',['../namespace_highway_1_1_data_1_1_entity_framework_1_1_mappings.html',1,'Highway::Data::EntityFramework']]],
+  ['mstest',['MSTest',['../namespace_highway_1_1_test_1_1_m_s_test.html',1,'Highway::Test']]],
   ['queryobjects',['QueryObjects',['../namespace_highway_1_1_data_1_1_query_objects.html',1,'Highway::Data']]],
   ['repositories',['Repositories',['../namespace_highway_1_1_data_1_1_entity_framework_1_1_repositories.html',1,'Highway::Data::EntityFramework']]],
-  ['services',['Services',['../namespace_highway_1_1_data_1_1_services.html',1,'Highway::Data']]]
+  ['services',['Services',['../namespace_highway_1_1_data_1_1_services.html',1,'Highway::Data']]],
+  ['test',['Test',['../namespace_highway_1_1_test.html',1,'Highway']]]
 ];

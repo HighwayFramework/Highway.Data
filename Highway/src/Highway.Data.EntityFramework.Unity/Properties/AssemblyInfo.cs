1243e75444da90a632224ab6f6fb4b7ce5f8f309
Devlin Liles <devlin@devlinliles.com>
2012-07-20 13:29:40 -0500

    Adding untity configurablity

--- Highway/src/Highway.Data.EntityFramework.Unity/Properties/AssemblyInfo.cs
+++ Highway/src/Highway.Data.EntityFramework.Unity/Properties/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9889132e-11cd-4e20-a0ab-270e1e47d232")]
 
+[assembly: AssemblyTitleAttribute("Highway.Data.EntityFramework.Unity")]

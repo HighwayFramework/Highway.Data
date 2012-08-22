2e3b39dbe1dcaaaf2ba19785acde084b0aaf6b65
Devlin Liles <Devlin@devlinliles.com>
2012-08-22 07:21:36 -0500

    works in progress

---
+++ Highway/src/Highway.Data.EntityFramework.Castle.Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using Highway.Data.Interfaces;
+using System;
+
+namespace Highway.Data.EntityFramework.Castle.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string SqlExpressConnectionString = @"Data Source=(local)\SQLExpress;Initial Catalog=HighwayDemo;Integrated Security=True";
+
+            container.Register(Component.For<IRepository>().ImplementedBy<EntityFrameworkRepository>(),
+                Component.For<IDataContext>().ImplementedBy<EntityFrameworkContext>().DependsOn(new { connectionString = SqlExpressConnectionString }),
+	            Component.For<IEventHandler>().ImplementedBy<EventHandler>(),
+                Component.For<IMappingConfiguration>().ImplementedBy<YourMappingClass>());
+
+            // Use for Demos
+            // DropCreateDatabaseIfModelChanges, Migrations not supported yet.  (IDatabaseInitializers)
+            //Database.SetInitializer(new DropCreateDatabaseAlways<EntityFrameworkContext>());
+
+            var application = ObjectFactory.GetInstance<DemoApplication>();
+            application.Run();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Highway/src/Highway.Data.EntityFramework.Ninject.Example/HighwayDataMappings.cs
@@ -0,0 +1,12 @@
+﻿using Highway.Data.EntityFramework.Ninject.Example.Domain;
+
+namespace Highway.Data.EntityFramework.Ninject.Example
+{
+    public class HighwayDataMappings : IMappingConfiguration
+    {
+        public void ConfigureModelBuilder(System.Data.Entity.DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().ToTable("People");
+        }
+    }
+}

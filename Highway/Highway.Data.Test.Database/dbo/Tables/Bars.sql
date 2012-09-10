f9c95ea3833b28a363b993b5c50540632f50c18d
Devlin Liles <Devlin@devlinliles.com>
2012-09-10 16:23:12 -0500

    Added Test Database for multiple model checks

---
+++ Highway/Highway.Data.Test.Database/dbo/Tables/Bars.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Bars] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Bars] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
--- Highway/test/Highway.Data.EntityFramework.Tests/Contexts/GivenAnAggregateContextFactory.cs
+++ Highway/test/Highway.Data.EntityFramework.Tests/Contexts/GivenAnAggregateContextFactory.cs
@@ -20,7 +20,8 @@ namespace Highway.Data.EntityFramework.Tests.UnitTests
     [TestClass]
     public class GivenAnAggregateContextFactory
     {
-        [TestMethod]
+        //INFO: Deploy the test database before running this
+        [TestMethod, TestCategory("Integration"), TestCategory("Database")]
         public void ShouldAddToTheCorrectContext()
         {
             // Arrange
@@ -48,6 +49,49 @@ namespace Highway.Data.EntityFramework.Tests.UnitTests
             Assert.IsTrue(objectContext.ObjectStateManager.GetObjectStateEntries(EntityState.Added).Count() == 2);
             Assert.IsTrue(objectContext.ObjectStateManager.GetObjectStateEntries(EntityState.Added).First().EntitySet.Name == "Foos");
             Assert.IsTrue(objectContext.ObjectStateManager.GetObjectStateEntries(EntityState.Added).Last().EntitySet.Name == "Bars");
+        }
+
+        [TestMethod, TestCategory("Integration"), TestCategory("Database")]
+        public void ShouldAllowTwoDifferentContextsToBeLoadedInTheSameAppDomain()
+        {
+            //Arrange
+            var container = new WindsorContainer();
+            var configuration = new AggregateConfiguration("Test", new IMappingConfiguration[] { new FooMappingConfiguration(), new BarMappingConfiguration() }, null, null, new[] { typeof(Foo), typeof(Bar) });
+            var secondConfiguration = new AggregateConfiguration("Test", new IMappingConfiguration[] { new FooMappingConfiguration() }, null, null, new[] { typeof(Foo) });
+
+            var typeName = string.Format("{0},{1}", typeof(Foo).FullName, typeof(Bar).FullName);
+            var secondTypeName = typeof(Foo).FullName;
+            container.Register(
+                Component.For<IAggregateConfiguration>().Instance(configuration).Named(typeName),
+                Component.For<IAggregateConfiguration>().Instance(secondConfiguration).Named(secondTypeName));
+            
+            var windsorServiceLocator = new WindsorServiceLocator(container);
+            ServiceLocator.SetLocatorProvider(() => windsorServiceLocator);
+
+            IDataContext context = AggregateContextFactory.Create<Foo, Bar>();
+            IDataContext contextTwo = AggregateContextFactory.Create<Foo>();
+            var foo = new Foo();
+            var bar = new Bar();
+
+            // Act
+            context.Add(foo);
+            context.Add(bar);
+
+            contextTwo.Add(foo);
+
+            // Assert
+            // peek under the covers and ensure that each add went
+            // to the right DbContext.
+            Assert.AreNotSame(context.GetType().FullName, contextTwo.GetType().FullName);
+            var objectContext = ((IObjectContextAdapter)context).ObjectContext;
+            Assert.IsTrue(objectContext.ObjectStateManager.GetObjectStateEntries(EntityState.Added).Count() == 2);
+            Assert.IsTrue(objectContext.ObjectStateManager.GetObjectStateEntries(EntityState.Added).First().EntitySet.Name == "Foos");
+            Assert.IsTrue(objectContext.ObjectStateManager.GetObjectStateEntries(EntityState.Added).Last().EntitySet.Name == "Bars");
+
+            var objectContextTwo = ((IObjectContextAdapter)contextTwo).ObjectContext;
+            Assert.IsTrue(objectContextTwo.ObjectStateManager.GetObjectStateEntries(EntityState.Added).Count() == 1);
+            Assert.IsTrue(objectContextTwo.ObjectStateManager.GetObjectStateEntries(EntityState.Added).Single().EntitySet.Name == "Foos");
+
         }
     }
 }

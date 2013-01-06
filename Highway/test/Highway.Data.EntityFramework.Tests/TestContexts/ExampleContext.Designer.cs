d9987bf61f71f2911a6d6d865420523704b45a45
Devlin Liles <devlin@devlinliles.com>
2013-01-05 18:28:31 -0600

    reverting to single framework builds for simplicity

--- Highway/test/Highway.Data.EntityFramework.Tests/TestContexts/ExampleContext.Designer.cs
+++ Highway/test/Highway.Data.EntityFramework.Tests/TestContexts/ExampleContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'F:\Code\Fun\Highway.Data\Highway\test\Highway.Data.EntityFramework.Tests\TestContexts\ExampleContext.edmx'. 
+﻿// Default code generation is disabled for model 'E:\Code\Fun\Highway.Data\Highway\test\Highway.Data.EntityFramework.Tests\TestContexts\ExampleContext.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.

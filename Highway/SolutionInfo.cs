3b9f3d66d631e6f93d82566ed31d25f364130c05
Devlin Liles <devlin@devlinliles.com>
2013-01-11 16:47:00 -0600

    Updating assembly version

--- Highway/SolutionInfo.cs
+++ Highway/SolutionInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.5.0.1")]
-[assembly: AssemblyFileVersion("3.5.0.1")]
-[assembly: AssemblyInformationalVersion("3.5.0.1-alpha")]
+[assembly: AssemblyVersion("3.6.0.0")]
+[assembly: AssemblyFileVersion("3.6.0.0")]
+[assembly: AssemblyInformationalVersion("3.6.0.0")]

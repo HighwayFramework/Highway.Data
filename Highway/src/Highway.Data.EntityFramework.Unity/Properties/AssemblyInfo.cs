d0dc24efc4ebe9ae279b882dced6e5d573c14b0f
Devlin Liles <devlin@devlinliles.com>
2013-02-20 17:59:40 -0600

    Removing unneeded projects

--- Highway/src/Highway.Data.EntityFramework.Unity/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("9889132e-11cd-4e20-a0ab-270e1e47d232")]
-[assembly: AssemblyTitle("Highway.Data.EntityFramework.Unity")]

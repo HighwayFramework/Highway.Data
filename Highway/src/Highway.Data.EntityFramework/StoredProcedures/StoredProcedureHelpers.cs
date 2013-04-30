e55acb25f7f53ba39c993e959469d34977f9095c
Devlin Liles <devlin@devlinliles.com>
2013-04-30 10:59:17 -0500

    Fixing a reference issue

--- Highway/src/Highway.Data.EntityFramework/StoredProcedures/StoredProcedureHelpers.cs
+++ Highway/src/Highway.Data.EntityFramework/StoredProcedures/StoredProcedureHelpers.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;

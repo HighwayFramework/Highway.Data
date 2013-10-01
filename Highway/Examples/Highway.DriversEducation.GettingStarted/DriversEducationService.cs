04a5a1725cc7f1eb5bbf6ab63e9392788dc775be
Devlin Liles <devlin@devlinliles.com>
2013-10-01 09:36:09 -0500

    Adding Examples Projects to keep track on the tutorial code

---
+++ Highway/Examples/Highway.DriversEducation.GettingStarted/DriversEducationService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Highway.Data;
+
+namespace Highway.DriversEducation.GettingStarted
+{
+    public class DriversEducationService
+    {
+        private readonly IRepository _repository;
+
+        public DriversEducationService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddDriver(string name)
+        {
+            _repository.Context.Add(new Driver(name));
+        }
+    }
+
+    public class Driver
+    {
+        public string Name { get; set; }
+
+        public Driver(string name)
+        {
+            Name = name;
+        }
+    }
+}

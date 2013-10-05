1ba4bfbcfd599919eb679021bd660d670cf04142
Tim Rayburn <Tim@TimRayburn.net>
2013-10-04 20:03:03 -0500

    Adding SNK

--- Highway/SolutionInfo.cs
+++ Highway/SolutionInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.0.5.3")]
-[assembly: AssemblyFileVersion("4.0.5.3")]
-[assembly: AssemblyInformationalVersion("4.0.5.3")]
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyInformationalVersion("4.1.0.0")]

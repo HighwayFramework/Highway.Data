94bb0e5f68210a532d52a423b41562beff0aea05
Devlin Liles <devlin@devlinliles.com>
2013-11-18 16:10:09 -0600

    Testing merge

--- Highway/test/Highway.Data.EntityFramework.Tests/AdvancedFeatures/TestDomain.cs
+++ Highway/test/Highway.Data.EntityFramework.Tests/AdvancedFeatures/TestDomain.cs
@@ -7,6 +7,9 @@ namespace Highway.Data.EntityFramework.Tests.EventManagement
     public class TestDomain : IDomain
     {
         public List<IInterceptor> Events { get; set; }
+
+        //This is not here
+
         public string ConnectionString { get; set; }
         public IMappingConfiguration Mappings { get; set; }
         public IContextConfiguration Context { get; set; }

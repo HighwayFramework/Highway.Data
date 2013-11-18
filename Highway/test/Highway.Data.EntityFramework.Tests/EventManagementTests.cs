c71f9200fad5a8583fd76888d36c1a76189aec0c
Devlin Liles <devlin@devlinliles.com>
2013-11-17 23:29:16 -0600

    Adding basic eventing changes

--- Highway/test/Highway.Data.EntityFramework.Tests/EventManagementTests.cs
+++ Highway/test/Highway.Data.EntityFramework.Tests/EventManagementTests.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System.Collections.Generic;
+using FluentAssertions;
 using Highway.Data.Domain;
 using Highway.Data.EntityFramework.Tests.AdvancedFeatures.EventManagement;
 using Highway.Data.EntityFramework.Tests.Properties;
@@ -16,7 +17,7 @@ namespace Highway.Data.EntityFramework.Tests.EventManagement
     public class EventManagementTests
     {
         [TestMethod]
-        public void ShouldRegisterOneEventHandlerForOneEvent()
+        public void ShouldRegisterOneEventHandlerForPreSave()
         {
             //arrange 
             var domain = new TestDomain();
@@ -33,6 +34,74 @@ namespace Highway.Data.EntityFramework.Tests.EventManagement
 
             //assert
             testPreSaveInterceptor.WasCalled.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void ShouldRegisterTwoEventHandlersForPreSaveAndHonorPriority()
+        {
+            //arrange 
+            var domain = new TestDomain();
+            var testPreSaveInterceptor = new TestPreSaveInterceptor(1);
+            var testPreSaveInterceptor2 = new TestPreSaveInterceptor(2);
+            domain.Events = new List<IInterceptor>
+            {
+                testPreSaveInterceptor,
+                testPreSaveInterceptor2,
+            };
+            domain.ConnectionString = Settings.Default.Connection;
+
+            //act
+            var context = new DomainContext<TestDomain>(domain);
+            context.Commit();
+
+            //assert
+            testPreSaveInterceptor.WasCalled.ShouldBeTrue();
+            testPreSaveInterceptor2.WasCalled.ShouldBeTrue();
+            testPreSaveInterceptor.CallTime.Should().BeBefore(testPreSaveInterceptor2.CallTime);
+        }
+
+        [TestMethod]
+        public void ShouldRegisterOneEventHandlerForPostSave()
+        {
+            //arrange 
+            var domain = new TestDomain();
+            var testPreSaveInterceptor = new TestPostSaveInterceptor(1);
+            domain.Events = new List<IInterceptor>
+            {
+                testPreSaveInterceptor
+            };
+            domain.ConnectionString = Settings.Default.Connection;
+
+            //act
+            var context = new DomainContext<TestDomain>(domain);
+            context.Commit();
+
+            //assert
+            testPreSaveInterceptor.WasCalled.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void ShouldRegisterTwoEventHandlersForPostSaveAndHonorPriority()
+        {
+            //arrange 
+            var domain = new TestDomain();
+            var testPreSaveInterceptor = new TestPostSaveInterceptor(1);
+            var testPreSaveInterceptor2 = new TestPostSaveInterceptor(2);
+            domain.Events = new List<IInterceptor>
+            {
+                testPreSaveInterceptor,
+                testPreSaveInterceptor2,
+            };
+            domain.ConnectionString = Settings.Default.Connection;
+
+            //act
+            var context = new DomainContext<TestDomain>(domain);
+            context.Commit();
+
+            //assert
+            testPreSaveInterceptor.WasCalled.ShouldBeTrue();
+            testPreSaveInterceptor2.WasCalled.ShouldBeTrue();
+            testPreSaveInterceptor.CallTime.Should().BeBefore(testPreSaveInterceptor2.CallTime);
         }
     }
 

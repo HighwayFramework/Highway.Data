b7050040ce6d6ffedc45ebb1fc102de92c0b2fa2
Devlin Liles <devlin@devlinliles.com>
2013-11-19 12:58:56 -0600

    Adding Testable Stub for configuration for Tim

---
+++ Highway/src/Highway.Data.EntityFramework/Configuration/DbModelBuilderAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Configuration.Types;
+using System.Data.Entity.Utilities;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Highway.Data
+{
+    public class DbModelBuilderAdapter : DbModelBuilder, IDbModelBuilderAdapter
+    {
+        public new ConfigurationRegistrarAdapater Configurations
+        {
+            get { return new ConfigurationRegistrarAdapater(base.Configurations); }
+        }
+    }
+}
---
+++ Highway/src/Highway.Data.EntityFramework/Configuration/IDbModelBuilderAdapter.cs
@@ -0,0 +1,10 @@
+﻿namespace Highway.Data
+{
+    /// <summary>
+    /// Testable interface for our adapter
+    /// </summary>
+    public interface IDbModelBuilderAdapter
+    {
+        new ConfigurationRegistrarAdapater Configurations { get; }
+    }
+}

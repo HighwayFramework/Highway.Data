861c8d7669d400224086a56ab8f1b85b3a94b8d1
Eric Burcham <eric@pixelninjas.com>
2014-03-19 12:28:20 -0500

    Added summary documentation.

--- Highway/src/Highway.Data/PrebuiltQueries/GetById.cs
+++ Highway/src/Highway.Data/PrebuiltQueries/GetById.cs
@@ -15,8 +15,9 @@ namespace Highway.Data
     public class GetById<TId, T> : Scalar<T> where T : class, IIdentifiable<TId> where TId : struct, IEquatable<TId>
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="GetById{TId,T}"/> class.
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">The Id of the <see cref="T"/> to return.</param>
         public GetById(TId id)
         {
             ContextQuery = context => context.AsQueryable<T>().FirstOrDefault(x => x.Id.Equals(id));

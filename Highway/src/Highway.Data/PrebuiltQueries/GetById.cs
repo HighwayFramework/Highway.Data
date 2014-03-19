dc18118a21ab87349f08237e97ece71dfeeab2f2
Eric Burcham <eric@pixelninjas.com>
2014-03-19 12:45:03 -0500

    Added documentation for IIdentifiable

--- Highway/src/Highway.Data/PrebuiltQueries/GetById.cs
+++ Highway/src/Highway.Data/PrebuiltQueries/GetById.cs
@@ -10,12 +10,12 @@ namespace Highway.Data
     /// <summary>
     ///     This pre-built query get a specific type by the Id provided
     /// </summary>
-    /// <typeparam name="TId">The type of ID.</typeparam>
+    /// <typeparam name="TId">The type of the Id.</typeparam>
     /// <typeparam name="T">The type to get.</typeparam>
     public class GetById<TId, T> : Scalar<T> where T : class, IIdentifiable<TId> where TId : struct, IEquatable<TId>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="GetById{TId,T}"/> class.
+        ///     Initializes a new instance of the <see cref="GetById{TId,T}"/> class.
         /// </summary>
         /// <param name="id">The Id of the <see cref="T"/> to return.</param>
         public GetById(TId id)
@@ -25,10 +25,14 @@ namespace Highway.Data
     }
 
     /// <summary>
+    ///     Defines a generalized Id property for identifying an individual entity.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">The type of the Id.</typeparam>
     public interface IIdentifiable<T> where T : IEquatable<T>
     {
+        /// <summary>
+        ///     Gets or sets a value identifying the individual entity.
+        /// </summary>
         T Id { get; set; }
     }
 }

ed0d21d07e814be52c195f15ec465f0d8efcf5cd
Long Mai <long.mai@improvingenterprises.com>
2014-03-18 22:00:01 -0500

    Add short identity strategy

---
+++ Highway/src/Highway.Data/Contexts/IdentityStrategy/ShortdentityStrategy.cs
@@ -0,0 +1,31 @@
+﻿#region
+
+using System;
+using System.Linq.Expressions;
+using System.Threading;
+
+#endregion
+
+namespace Highway.Data.Contexts
+{
+    public class ShortIdentityStrategy<T> : IdentityStrategy<T, short>
+        where T : class
+    {
+        static Object lockObject = new Object();
+
+        static ShortIdentityStrategy()
+        {
+            Generator = GenerateShort;
+        }
+
+        public ShortIdentityStrategy(Expression<Func<T, short>> property)
+            : base(property)
+        {
+        }
+
+        private static short GenerateShort()
+        {
+            lock (lockObject) { return ++LastValue; }
+        }
+    }
+}

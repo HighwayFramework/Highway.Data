73f6925436b0985fa37adf6fd0784230d1db8a94
Long Mai <long.mai@improvingenterprises.com>
2014-03-18 21:46:06 -0500

    Add long identity strategy

---
+++ Highway/src/Highway.Data/Contexts/IdentityStrategy/LongIdentityStrategy.cs
@@ -0,0 +1,29 @@
+﻿#region
+
+using System;
+using System.Linq.Expressions;
+using System.Threading;
+
+#endregion
+
+namespace Highway.Data.Contexts
+{
+    public class LongIdentityStrategy<T> : IdentityStrategy<T, long>
+        where T : class
+    {
+        static LongIdentityStrategy()
+        {
+            Generator = GenerateLong;
+        }
+
+        public LongIdentityStrategy(Expression<Func<T, long>> property)
+            : base(property)
+        {
+        }
+
+        private static long GenerateLong()
+        {
+            return Interlocked.Increment(ref LastValue);
+        }
+    }
+}

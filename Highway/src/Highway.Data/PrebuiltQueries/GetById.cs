47db4f923e088fca94e23a3cd2c31c2a65b816e6
Eric Burcham <eric@pixelninjas.com>
2014-03-19 12:46:46 -0500

    Moved IIdentifiable into the Interfaces folder.

--- Highway/src/Highway.Data/PrebuiltQueries/GetById.cs
+++ Highway/src/Highway.Data/PrebuiltQueries/GetById.cs
@@ -8,7 +8,7 @@ using System.Linq;
 namespace Highway.Data
 {
     /// <summary>
-    ///     This pre-built query get a specific type by the Id provided
+    ///     This pre-built query get a specific type by the Id provided.
     /// </summary>
     /// <typeparam name="TId">The type of the Id.</typeparam>
     /// <typeparam name="T">The type to get.</typeparam>
@@ -22,17 +22,5 @@ namespace Highway.Data
         {
             ContextQuery = context => context.AsQueryable<T>().FirstOrDefault(x => x.Id.Equals(id));
         }
-    }
-
-    /// <summary>
-    ///     Defines a generalized Id property for identifying an individual entity.
-    /// </summary>
-    /// <typeparam name="T">The type of the Id.</typeparam>
-    public interface IIdentifiable<T> where T : IEquatable<T>
-    {
-        /// <summary>
-        ///     Gets or sets a value identifying the individual entity.
-        /// </summary>
-        T Id { get; set; }
     }
 }

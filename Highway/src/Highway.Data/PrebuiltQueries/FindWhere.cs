982b2a9032960b07d2f776e2185a96893aa66ece
Philip Mateescu <hello@philipm.at>
2014-06-07 21:13:56 -0500

    a query that filters by an arbitrary where condition

---
+++ Highway/src/Highway.Data/PrebuiltQueries/FindWhere.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Highway.Data.PrebuiltQueries
+{
+    /// <summary>
+    /// Finds all items of a certain type that meet a specified condition
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <example>
+    /// new FindWhere(person => person.Name == "Philip")
+    /// </example>
+    public class FindWhere<T> : Query<T> where T : class
+    {
+        public FindWhere(Expression<Func<T, bool>> whereCondition) {
+            ContextQuery = context => context.AsQueryable<T>()
+                .Where(whereCondition);
+        }
+
+        private FindWhere(Func<IDataContext, IQueryable<T>> contextFilter) {
+            ContextQuery = contextFilter;
+        }
+
+        /// <summary>
+        /// Creates a FindWhere query with results sorted by a given criterion.
+        /// </summary>
+        /// <typeparam name="TKey">The Type of the item being sorted by</typeparam>
+        /// <param name="whereCondition">The filtering condition</param>
+        /// <param name="orderBy">The ordering criterion</param>
+        /// <example>
+        /// FindWhere.OrderedBy(
+        ///                     person => person.Name == "Philip", 
+        ///                     person => person.DateOfBirth);
+        /// </example>
+        public static FindWhere<T> OrderedBy<TKey>(
+            Expression<Func<T, bool>> whereCondition,
+            Expression<Func<T, TKey>> orderBy) {
+            Func<IDataContext, IQueryable<T>> contextFilter;
+            contextFilter = context => context.AsQueryable<T>()
+                .OrderBy(orderBy)
+                .Where(whereCondition);
+            return new FindWhere<T>(contextFilter);
+        }
+    }
+}
---
+++ Highway/test/Highway.Data.EntityFramework.Tests/Queries/FindWhereTests.cs
@@ -0,0 +1,75 @@
+﻿#region
+
+using System.Linq;
+using FluentAssertions;
+using Highway.Data.Contexts;
+using Highway.Data.Tests.InMemory.Domain;
+using Highway.Data.Tests.InMemory.ScenarioTests.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Highway.Data.PrebuiltQueries;
+
+#endregion
+
+namespace Highway.Data.Tests.InMemory.ScenarioTests
+{
+    [TestClass]
+    public class FindWhereTests
+    {
+        private IRepository repo;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            repo = new Repository(new InMemoryDataContext());
+
+            var author = new Author
+            {
+                FirstName = "Devlin",
+                LastName = "Liles",
+                Email = "devlin@devlinliles.com",
+                TwitterHandle = "@DevlinLiles"
+            };
+
+
+            //Act - Scenario
+            var blogService = new TestBlogService(repo);
+            blogService.StartBlog("Testing", author);
+
+            blogService.Post("Testing", new Post
+            {
+                Title = "Test One",
+                Body = "This is a body paragraph"
+            });
+
+            blogService.Post("Testing", new Post
+            {
+                Title = "Test Two",
+                Body = "This is a body paragraph"
+            });
+
+            blogService.Post("Testing", new Post
+            {
+                Title = "Test First",
+                Body = "This is a body paragraph"
+            });
+        }
+
+        [TestMethod]
+        public void ShouldFindByWhereCondition()
+        {
+            var posts = repo.Find(new FindWhere<Post>(p => p.Title == "Test Two"));
+            posts.Count().Should().Be(1);
+        }
+
+        [TestMethod]
+        public void ShouldOrderTheResults()
+        {
+            var posts = repo.Find(FindWhere<Post>.OrderedBy(
+                p => p.Title.StartsWith("Test") && !p.Title.Contains("Two"),
+                p => p.Title));
+            posts.Count().Should().Be(2);
+            posts.First().Title.Should().Be("Test First");
+            posts.Last().Title.Should().Be("Test One");
+        }
+    }
+}

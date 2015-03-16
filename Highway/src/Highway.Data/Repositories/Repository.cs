6464a106a0b10f11ac494055afe10922e4707250
Devlin Liles <dliles@eprod.com>
2015-03-16 10:29:08 -0500

    Adding Async Methods to Repository

--- Highway/src/Highway.Data/Repositories/Repository.cs
+++ Highway/src/Highway.Data/Repositories/Repository.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace Highway.Data
 {
@@ -28,6 +30,15 @@ namespace Highway.Data
         }
 
         /// <summary>
+        ///     Executes a prebuilt <see cref="ICommand" />
+        /// </summary>
+        /// <param name="command">The prebuilt command object</param>
+        public virtual void Execute(ICommand command)
+        {
+            ExecuteAsync(command).Wait();
+        }
+
+        /// <summary>
         ///     Executes a prebuilt <see cref="IScalar{T}" /> and returns a single instance of <typeparamref name="T" />
         /// </summary>
         /// <typeparam name="T">The Entity being queried</typeparam>
@@ -35,16 +46,7 @@ namespace Highway.Data
         /// <returns>The instance of <typeparamref name="T" /> returned from the query</returns>
         public virtual T Find<T>(IScalar<T> query)
         {
-            return query.Execute(_context);
-        }
-
-        /// <summary>
-        ///     Executes a prebuilt <see cref="ICommand" />
-        /// </summary>
-        /// <param name="command">The prebuilt command object</param>
-        public virtual void Execute(ICommand command)
-        {
-            command.Execute(_context);
+            return FindAsync(query).Result;
         }
 
         /// <summary>
@@ -55,7 +57,7 @@ namespace Highway.Data
         /// <returns>The <see cref="IEnumerable{T}" /> returned from the query</returns>
         public virtual IEnumerable<T> Find<T>(IQuery<T> query)
         {
-            return query.Execute(_context);
+            return FindAsync(query).Result;
         }
 
         /// <summary>
@@ -67,7 +69,58 @@ namespace Highway.Data
         public virtual IEnumerable<IProjection> Find<TSelection, IProjection>(IQuery<TSelection, IProjection> query)
             where TSelection : class
         {
-            return query.Execute(_context);
+            return FindAsync(query).Result;
+        }
+
+        /// <summary>
+        ///     Executes a prebuilt <see cref="ICommand" /> asynchronously
+        /// </summary>
+        /// <param name="command">The prebuilt command object</param>
+        public virtual Task ExecuteAsync(ICommand command)
+        {
+            var task = new Task(() => command.Execute(_context));
+            task.Start();
+            return task;
+        }
+
+        /// <summary>
+        ///     Executes a prebuilt <see cref="IScalar{T}" /> and returns a single instance of <typeparamref name="T" />
+        /// </summary>
+        /// <typeparam name="T">The Entity being queried</typeparam>
+        /// <param name="query">The prebuilt Query Object</param>
+        /// <returns>The task that will return an instance of <typeparamref name="T" /> from the query</returns>
+        public virtual Task<T> FindAsync<T>(IScalar<T> query)
+        {
+            var task = new Task<T>(() => query.Execute(_context));
+            task.Start();
+            return task;
+        }
+
+        /// <summary>
+        ///     Executes a prebuilt <see cref="IQuery{T}" /> and returns an <see cref="IEnumerable{T}" />
+        /// </summary>
+        /// <typeparam name="T">The Entity being queried</typeparam>
+        /// <param name="query">The prebuilt Query Object</param>
+        /// <returns>The task that will return <see cref="IEnumerable{T}" /> from the query</returns>
+        public virtual Task<IEnumerable<T>> FindAsync<T>(IQuery<T> query)
+        {
+            var task = new Task<IEnumerable<T>>(() => query.Execute(_context));
+            task.Start();
+            return task;
+        }
+
+        /// <summary>
+        ///     Executes a prebuilt <see cref="IQuery{T}" /> and returns an <see cref="IEnumerable{T}" />
+        /// </summary>
+        /// <typeparam name="T">The Entity being queried</typeparam>
+        /// <param name="query">The prebuilt Query Object</param>
+        /// <returns>The task that will return <see cref="IEnumerable{T}" /> from the query</returns>
+        public virtual Task<IEnumerable<IProjection>> FindAsync<TSelection, IProjection>(IQuery<TSelection, IProjection> query)
+            where TSelection : class
+        {
+            var task = new Task<IEnumerable<IProjection>>(() => query.Execute(_context));
+            task.Start();
+            return task;
         }
     }
 }

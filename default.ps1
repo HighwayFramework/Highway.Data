0e2458d6ff08a8831be69206c9b6cc06c98d33ec
Devlin Liles <dliles@eprod.com>
2015-07-07 08:32:15 -0500

    Removing security from base packages, and validating tests before publish

--- default.ps1
+++ default.ps1
@@ -11,7 +11,7 @@ properties {
     $build_config = "Release"
     $pack_dir = ".\pack"
     $build_archive = ".\buildarchive\"
-    $version_number = "5.2.0.0"
+    $version_number = "6.0.0.0"
     $nuget_version_number = $version_number
     if ($Env:BUILD_NUMBER -ne $null) {
         $nuget_version_number += "-$Env:BUILD_NUMBER"

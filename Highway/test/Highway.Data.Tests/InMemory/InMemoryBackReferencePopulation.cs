d795544d1eb7b65e30fa9556bd8b5c1ed425edbe
Devlin Liles <capscdev@hotmail.com>
2015-07-17 06:33:41 -0500

    Fixed the null invocation for adding to back referencing collections on the in memory context

---
+++ Highway/test/Highway.Data.Tests/InMemory/InMemoryBackReferencePopulation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Highway.Data.Contexts;
+using Highway.Data.Tests.InMemory.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Highway.Data.Tests.InMemory
+{
+    [TestClass]
+    public class InMemoryBackReferencePopulation
+    {
+        [TestMethod]
+        public void ShouldPopulateSingleReference()
+        {
+            var context = new InMemoryDataContext();
+
+            var child = new Post();
+            var blog = new Blog("Test");
+            blog.Posts.Add(child);
+
+            context.Add(blog);
+            context.Commit();
+
+            Assert.IsNotNull(child.Blog);
+        }
+
+        [TestMethod]
+        public void ShouldPopulateCollectionBasedReference()
+        {
+            var context = new InMemoryDataContext();
+
+            var child = new Post();
+            var blog = new Blog("Test");
+            child.Blog = blog;
+
+            context.Add(child);
+            context.Commit();
+
+            Assert.AreEqual(1, blog.Posts.Count(x => x == child));
+        }
+
+        [TestMethod]
+        public void ShouldPopulateCollectionBasedReferenceReplacingNullCollection()
+        {
+            var context = new InMemoryDataContext();
+
+            var child = new Post();
+            var blog = new Blog("Test");
+            child.Blog = blog;
+            blog.Posts = null;
+
+            context.Add(child);
+            context.Commit();
+
+            Assert.AreEqual(1, blog.Posts.Count(x => x == child));
+        }
+    }
+}

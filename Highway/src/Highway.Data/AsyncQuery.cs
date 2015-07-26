2e2a179f208e2a4a0b12621be2ff66ede818c3ad
Devlin Liles <capscdev@hotmail.com>
2015-07-26 13:36:52 -0500

    Re-designing the async workflow in highway.data:

---
+++ Highway/src/Highway.Data/AsyncQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Highway.Data
+{
+    public class AsyncQuery<T> : IAsyncQuery<T>
+    {
+        protected Expression<Func<IDataContext, Task>> ContextQuery;
+
+        public Task<IEnumerable<T>> Execute(IDataContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IAsyncQuery<T>
+    {
+        Task<IEnumerable<T>> Execute(IDataContext context);
+    }
+}

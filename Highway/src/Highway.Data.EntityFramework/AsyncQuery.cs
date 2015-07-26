74b53f6ba863fc79cc0f85d3b5df9b3a351ff93d
Devlin Liles <capscdev@hotmail.com>
2015-07-26 14:55:45 -0500

    Reworking async design

---
+++ Highway/src/Highway.Data.EntityFramework/AsyncQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Highway.Data.EntityFramework
+{
+    public class AsyncQuery<T> : IAsyncQuery<T>
+    {
+        protected Func<IDataContext, IQueryable<T>> ContextQuery;
+
+        public Task<IEnumerable<T>> Execute(IDataContext context)
+        {
+           return ContextQuery(context)
+               .ToListAsync()
+               .ContinueWith(x => x.Result as IEnumerable<T>);
+        }
+    }
+}
---
+++ Highway/src/Highway.Data/Interfaces/Queries/IAsyncQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Highway.Data
+{
+    public interface IAsyncQuery<T>
+    {
+        Task<IEnumerable<T>> Execute(IDataContext context);
+    }
+}

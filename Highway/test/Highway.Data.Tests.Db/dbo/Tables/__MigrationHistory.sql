4783bc5a40a4b137e39791aceab98a495bdf7256
Devlin Liles <dliles@eprod.com>
2015-10-13 09:21:31 -0500

    Updating async processing

--- Highway/test/Highway.Data.Tests.Db/dbo/Tables/__MigrationHistory.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[__MigrationHistory] (
-    [MigrationId]    NVARCHAR (150)  NOT NULL,
-    [ContextKey]     NVARCHAR (300)  NOT NULL,
-    [Model]          VARBINARY (MAX) NOT NULL,
-    [ProductVersion] NVARCHAR (32)   NOT NULL,
-    CONSTRAINT [PK_dbo.__MigrationHistory] PRIMARY KEY CLUSTERED ([MigrationId] ASC, [ContextKey] ASC)
-);
-

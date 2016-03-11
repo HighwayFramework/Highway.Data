bb18f0daea64ac4d29947939cbcd1e0044da0fae
Tim Rayburn <tim@timrayburn.net>
2016-03-11 01:54:41 -0600

    Testing Structure in place for CoreCLR

---
+++ coreclr/src/Highway.Data.InMemory/Class1.cs
@@ -0,0 +1,4 @@
+public class Class1
+{
+    // This is just a temporary file to make sure this all compiles.
+}
---
+++ coreclr/test/Highway.Data.Tests/RepositoryTests.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace Highway.Data.Tests
+{
+    [TestFixture]
+    public class RepositoryTests
+    {
+        [Test]
+        public void This_Should_Fail()
+        {
+            Assert.Fail("Nothing to see here");
+        }
+    }
+}

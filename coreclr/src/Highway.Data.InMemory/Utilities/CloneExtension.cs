183dbab9d478c57f28a2e628576b2664811aebb7
Tim Rayburn <tim@timrayburn.net>
2016-03-11 10:28:36 -0600

    Testing Changeup

--- coreclr/src/Highway.Data.InMemory/Utilities/CloneExtension.cs
+++ coreclr/src/Highway.Data.InMemory/Utilities/CloneExtension.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Highway.Data.InMemory.Utilities
 {
--- coreclr/test/Highway.Data.DotNet.Tests/Program.cs
+++ coreclr/test/Highway.Data.DotNet.Tests/Program.cs
@@ -2,7 +2,7 @@ using NUnitLite;
 using System;
 using System.Reflection;
 
-namespace Highway.Data.Tests
+namespace Highway.Data.DotNet.Tests
 {
     public class Program
     {

5359f072b931ae3db9eeda89391d60caf6d4da16
Tim Rayburn <tim@timrayburn.net>
2016-03-11 10:57:47 -0600

    InMemoryDataContextTests running

---
+++ coreclr/src/Highway.Data.InMemory/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Highway.Data.CoreCLR.Tests")]
---
+++ coreclr/test/Highway.Data.CoreCLR.Tests/InMemory/Domain/Site.cs
@@ -0,0 +1,8 @@
+namespace Highway.Data.CoreCLR.Tests.InMemory.Domain
+{
+    public class Site
+    {
+        public Blog Blog { get; set; }
+        public int Id { get; set; }
+    }
+}

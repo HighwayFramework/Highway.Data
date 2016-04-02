9672ffa6459ab01066f57329e556284930bb461c
Tim Rayburn <tim@timrayburn.net>
2016-04-01 23:10:46 -0500

    build ps1 in addition to sh

---
+++ coreclr/scripts/build.ps1
@@ -0,0 +1,34 @@
+#------------------------------------------------------------------------------
+# Full .NET Runtime Build & Test
+#------------------------------------------------------------------------------
+
+dnvm use 1.0.0-rc1-update2 -r clr
+dnu restore --quiet
+gci -recurse project.json | % {
+  Write-Host -------------------------------------------------------------------------
+  Write-Host "CLR Build $_"
+  dnu build "$_" --framework dnx451 --quiet
+}
+pushd .\test\Highway.Data.DotNet.Tests
+dnx test
+popd
+pushd .\test\Highway.Data.CoreCLR.Tests
+dnx test
+popd
+
+#------------------------------------------------------------------------------
+# CoreCLR Build & Test
+#------------------------------------------------------------------------------
+
+dnvm use 1.0.0-rc1-update2 -r coreclr
+dnu restore --quiet
+$exclude = get-content exclude-coreclr.txt
+$bashlikepaths = gci -recurse project.json | % { $_.FullName.Replace("$PWD",".").Replace("\","/") }
+$bashlikepaths | ? { $exclude -notcontains $_ } | % {
+  Write-Host -------------------------------------------------------------------------
+  Write-Host "CoreCLR Build $_"
+  dnu build "$_" --framework dnxcore50 --quiet
+}
+pushd .\test\Highway.Data.CoreCLR.Tests
+dnx test
+popd

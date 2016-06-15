897a9509c05ecbd4a9e49973d319ae2558b9ff89
Michael L Perry <michael@qedcode.com>
2016-06-15 11:35:15 -0500

    Display meaningful error messages.
    Entity validation exceptions are unparsable. Use the ValidateAndCommit
    extension method to turn them into useful, actionable messages.

---
+++ Highway/src/Highway.Data.EntityFramework/Extensions/DbContextExtentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+namespace Highway.Data.EntityFramework
+{
+    public static class DbContextExtentions
+    {
+        public static void ValidateAndCommit(this IUnitOfWork context)
+        {
+            try
+            {
+                context.Commit();
+            }
+            catch (DataException x)
+            {
+                string fullMessage = x.FullMessage();
+                throw new InvalidOperationException(fullMessage, x);
+            }
+        }
+    }
+}
---
+++ Highway/src/Highway.Data.EntityFramework/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+
+namespace Highway.Data.EntityFramework
+{
+    public static class ExceptionExtensions
+    {
+        public static string FullMessage(this Exception exception)
+        {
+            return String.Join(" ", exception
+                .NestedExceptions()
+                .Select(x => x.MeaningfulMessage()));
+        }
+
+        public static string MeaningfulMessage(this Exception exception)
+        {
+            var result = exception.Message;
+
+            var validationException = exception as DbEntityValidationException;
+
+            if (validationException != null)
+            {
+                var errors =
+                    from entity in validationException.EntityValidationErrors
+                    from error in entity.ValidationErrors
+                    select $"{error.ErrorMessage} Property name: {entity.Entry.Entity.GetType().Name}.{error.PropertyName}.";
+
+                result = String.Join(", ", errors);
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<Exception> NestedExceptions(this Exception exception)
+        {
+            while (exception != null)
+            {
+                yield return exception;
+                exception = exception.InnerException;
+            }
+        }
+    }
+}

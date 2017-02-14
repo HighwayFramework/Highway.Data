5eb78bf73a2ba77f4118e6cc0c7cc39650785917
Tim Rayburn <Tim Rayburn>
2017-02-13 16:53:28 -0800

    Working EFCore Implementation BASIC

---
+++ coreclr/Highway.Data.EntityFrameworkCore/DataContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Highway.Data
+{
+	public class DataContext : DbContext, IDataContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public IQueryable<T> AsQueryable<T>() where T : class
+		{
+			return this.Set<T>();
+		}
+
+		public int Commit()
+		{
+			return this.SaveChanges();
+		}
+
+		public Task<int> CommitAsync()
+		{
+			return this.SaveChangesAsync();
+		}
+
+		public T Reload<T>(T item) where T : class
+		{
+			this.ChangeTracker.Entries<T>()
+				.Single(ee => ee.Entity == item)
+				.Reload();
+			return item;
+		}
+
+		T IUnitOfWork.Add<T>(T item)
+		{
+			this.Add<T>(item);
+			return item;
+		}
+
+		T IUnitOfWork.Remove<T>(T item)
+		{
+			this.Remove<T>(item);
+			return item;
+		}
+
+		T IUnitOfWork.Update<T>(T item)
+		{
+			this.Update<T>(item);
+			return item;
+		}
+	}
+}
---
+++ coreclr/Highway.Data.Test/EntityFrameworkCoreInMemory.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.Linq;
+
+namespace Highway.Data.Test
+{
+	public class Person
+	{
+		public Guid Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+	}
+
+	public class FirstRayburn : Scalar<Person>
+	{
+		public FirstRayburn()
+		{
+			ContextQuery = c => c.AsQueryable<Person>()
+				.First(e => e.LastName == "Rayburn");
+		}
+	}
+	[TestClass]
+    public class EntityFrameworkCoreInMemory
+    {
+        [TestMethod]
+        public void ShouldStoreAndRetrive()
+        {
+			var mb = new ModelBuilder(new ConventionSet());
+			mb.Entity<Person>(e => e.HasKey(x => x.Id));
+			var builder = new DbContextOptionsBuilder()
+				.UseInMemoryDatabase()
+				.UseModel(mb.Model);
+			IDataContext dc = new DataContext(builder.Options);
+			IRepository repo = new Repository(dc);
+
+			repo.Context.Add(new Person { FirstName = "Tim", LastName = "Rayburn" });
+			repo.Context.Commit();
+
+			Assert.AreEqual("Tim", repo.Find(new FirstRayburn()).FirstName);
+        }
+    }
+}

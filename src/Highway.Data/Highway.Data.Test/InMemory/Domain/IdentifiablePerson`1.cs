18dadf08747430f40219b91758ff76f086d78831
Tim Rayburn <tim@timrayburn.net>
2019-09-04 08:33:59 -0500

    InMemoryDataContext and tests

---
+++ src/Highway.Data/Highway.Data.Test/InMemory/Domain/IdentifiablePerson`1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Highway.Data.Tests.InMemory.Domain
+{
+    public class IdentifiablePerson<T> : IIdentifiable<T> where T : IEquatable<T>
+    {
+        public T Id { get; set; }
+    }
+}

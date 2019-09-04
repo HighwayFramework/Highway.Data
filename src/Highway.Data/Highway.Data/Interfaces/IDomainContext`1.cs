63a9c2b8373d893d8c35bfcd4df80b65b50d174c
Tim Rayburn <tim@timrayburn.net>
2019-09-04 07:02:20 -0500

    Start of .NET Standard codebase

---
+++ src/Highway.Data/Highway.Data/Interfaces/IDomainContext`1.cs
@@ -0,0 +1,24 @@
+﻿using Highway.Data.Interceptors.Events;
+using System;
+using System.Linq;
+
+
+namespace Highway.Data
+{
+    /// <summary>
+    /// Contract for a Domain Context
+    /// </summary>
+    /// <typeparam name="T">The type of the Entity</typeparam>
+    public interface IDomainContext<in T> : IDataContext where T : class
+    {
+        /// <summary>
+        ///     The event fired just before the commit of the persistence
+        /// </summary>
+        event EventHandler<BeforeSave> BeforeSave;
+
+        /// <summary>
+        ///     The event fired just after the commit of the persistence
+        /// </summary>
+        event EventHandler<AfterSave> AfterSave;
+    }
+}
---
+++ src/Highway.Data/Highway.Data/Interfaces/IDomainRepository`1.cs
@@ -0,0 +1,27 @@
+﻿using Highway.Data.Interceptors.Events;
+using System;
+using System.Linq;
+
+
+namespace Highway.Data
+{
+    public interface IDomainRepository<in T> where T : class
+    {
+        event EventHandler<BeforeQuery> BeforeQuery;
+
+        event EventHandler<BeforeScalar> BeforeScalar;
+
+        event EventHandler<BeforeCommand> BeforeCommand;
+
+        event EventHandler<AfterQuery> AfterQuery;
+
+        event EventHandler<AfterScalar> AfterScalar;
+
+        event EventHandler<AfterCommand> AfterCommand;
+
+        /// <summary>
+        /// Gets the contract for a Domain Context
+        /// </summary>
+        IDomainContext<T> DomainContext { get; }
+    }
+}

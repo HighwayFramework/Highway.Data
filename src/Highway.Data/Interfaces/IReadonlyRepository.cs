d78f42d9b6d9730af420128267d4d5051ed607f6
Eric Burcham <Eric Burcham>
2021-07-15 19:03:07 -0500

    Create IReadonlyRepository

---
+++ src/Highway.Data/Interfaces/IReadonlyRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Highway.Data
+{
+    public interface IReadonlyRepository
+    {
+        /// <summary>
+        ///     Executes a prebuilt <see cref="ICommand" />
+        /// </summary>
+        /// <param name="command">The prebuilt command object</param>
+        void Execute(ICommand command);
+
+        /// <summary>
+        ///     Executes a prebuilt <see cref="ICommand" /> asynchronously
+        /// </summary>
+        /// <param name="command">The prebuilt command object</param>
+        Task ExecuteAsync(ICommand command);
+    }
+}
--- src/Highway.Data/Interfaces/IRepository.cs
+++ src/Highway.Data/Interfaces/IRepository.cs
@@ -7,7 +7,7 @@ namespace Highway.Data
     /// <summary>
     ///     The interface used to interact with the ORM Specific Implementations
     /// </summary>
-    public interface IRepository
+    public interface IRepository : IReadonlyRepository
     {
         /// <summary>
         ///     Reference to the Context the repository, allowing for create, update, and delete
@@ -29,18 +29,6 @@ namespace Highway.Data
         /// <param name="query">The prebuilt Query Object</param>
         /// <returns>The instance of <typeparamref name="T" /> returned from the query</returns>
         T Find<T>(IScalar<T> query);
-
-        /// <summary>
-        ///     Executes a prebuilt <see cref="ICommand" />
-        /// </summary>
-        /// <param name="command">The prebuilt command object</param>
-        void Execute(ICommand command);
-
-        /// <summary>
-        ///     Executes a prebuilt <see cref="ICommand" /> asynchronously
-        /// </summary>
-        /// <param name="command">The prebuilt command object</param>
-        Task ExecuteAsync(ICommand command);
 
         /// <summary>
         ///     Executes a prebuilt <see cref="IScalar{T}" /> and returns a single instance of <typeparamref name="T" />

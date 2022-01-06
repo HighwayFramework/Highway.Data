0271969b7e01d1bae5b281dbbf5087dbd04cf4e8
Eric Burcham <Eric Burcham>
2022-01-06 16:17:50 -0600

    reorder inheritance markers

--- src/Highway.Data/Interfaces/IDataContext.cs
+++ src/Highway.Data/Interfaces/IDataContext.cs
@@ -7,7 +7,7 @@ namespace Highway.Data
     /// <summary>
     ///     The standard interface used to interact with an ORM specific implementation
     /// </summary>
-    public interface IDataContext : IDisposable, IDataContextBase, IUnitOfWork
+    public interface IDataContext : IDataContextBase, IDisposable, IUnitOfWork
     {
         /// <summary>
         ///     The event fired just before the commit of the persistence
--- src/Highway.Data/Interfaces/IReadonlyDataContext.cs
+++ src/Highway.Data/Interfaces/IReadonlyDataContext.cs
@@ -5,7 +5,7 @@ namespace Highway.Data
     /// <summary>
     ///     Contract for a readonly Data Context
     /// </summary>
-    public interface IReadonlyDataContext : IDisposable, IDataContextBase
+    public interface IReadonlyDataContext : IDataContextBase, IDisposable
     {
     }
 }

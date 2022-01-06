88578631a340e2d7773b4cacb702c184a96cd3cd
Eric Burcham <Eric Burcham>
2022-01-05 22:03:00 -0600

    add skeleton of ReadonlyRepository

---
+++ src/Highway.Data/Repositories/ReadonlyRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Highway.Data
+{
+    public class ReadonlyRepository : IReadonlyRepository
+    {
+        public IReadonlyDataContext Context { get; }
+
+        public T Find<T>(IScalar<T> scalar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find<T>(IQuery<T> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindAsync<T>(IScalar<T> scalar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> FindAsync<T>(IQuery<T> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TProjection>> FindAsync<TSelection, TProjection>(IQuery<TSelection, TProjection> query)
+            where TSelection : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

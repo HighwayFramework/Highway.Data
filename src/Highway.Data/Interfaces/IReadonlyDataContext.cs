4a009666b0cdd388e5c8fd010a2902adaac18def
Eric Burcham <Eric Burcham>
2022-01-05 20:34:44 -0600

    Add IReadonlyDataContext

---
+++ src/Highway.Data/Interfaces/IReadonlyDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Highway.Data
+{
+    public interface IReadonlyDataContext : IDisposable
+    {
+        /// <summary>
+        ///     This gives a mock-able wrapper around normal Set method that allows for testability
+        /// </summary>
+        /// <typeparam name="T">The Entity being queried</typeparam>
+        /// <returns>
+        ///     <see cref="IQueryable{T}" />
+        /// </returns>
+        IQueryable<T> AsQueryable<T>()
+            where T : class;
+    }
+}

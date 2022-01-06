e52fa8f9f5961ea52feeb6e46cb0aa821d2d1c37
Eric Burcham <Eric Burcham>
2022-01-05 20:51:55 -0600

    Add IReadonlyDomainRepositoryFactory

---
+++ src/Highway.Data.EntityFramework/Factories/IReadonlyDomainRepositoryFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Highway.Data.Factories
+{
+    /// <summary>
+    ///     Interface for the factories of <see cref="IReadonlyRepository" />
+    /// </summary>
+    public interface IReadonlyDomainRepositoryFactory
+    {
+        /// <summary>
+        ///     Creates a readonly repository for the specified <see cref="IDomain" />
+        /// </summary>
+        /// <typeparam name="T">Domain for readonly repository</typeparam>
+        /// <returns>
+        ///     <see cref="IReadonlyRepository" />
+        /// </returns>
+        IReadonlyRepository CreateReadonly<T>()
+            where T : class, IDomain;
+
+        /// <summary>
+        ///     Creates a readonly repository for the specified <paramref name="type" />
+        /// </summary>
+        /// <param name="type">Type for readonly repository</param>
+        /// <returns>
+        ///     <see cref="IReadonlyRepository" />
+        /// </returns>
+        IReadonlyRepository CreateReadonly<T>(Type type);
+
+    }
+}
